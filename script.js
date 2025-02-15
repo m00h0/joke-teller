219f75805625e3940ad88aa369754eba43270302
newtonah0 <ezmh015@gmail.com>
2025-02-16 00:23:55 +0300

    fix linters errors

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function tellMe(joke) {
 
 async function getJoke() {
   let joke = '';
-  const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+  const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
